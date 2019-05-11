62930934d65f0ee18bd7b69559f74febf7f1c920
Clément Acher <clement.acher@gmail.com>
2019-05-11 16:16:14 +0200

    use click for the cli

--- main.py
+++ main.py
@@ -1,83 +1,81 @@
-import argparse
 from os import path
-from tqdm import tqdm
 
 from parser.parser import PCFG
 
+import click
+from tqdm import tqdm
+
 SCRIPT_PATH = path.dirname(path.abspath(__file__))
-OUTPUT_FOLDER = path.join(SCRIPT_PATH, './data')
 
 
-def parse_from_std(args):
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+@click.argument('lines_to_parse', type=str)
+@click.option(
+    '--training_set_path',
+    type=click.Path(),
+    default=path.join(SCRIPT_PATH, './data/train.tag'),
+    help='Path to the training dataset.')
+@click.option('--pprint', is_flag=True, help='Pretty print the result.')
+def parse_from_std(lines_to_parse, training_set_path, pprint):
     pcfg = PCFG()
-    pcfg.fill_pcfg_from_file(args.train_path)
+    pcfg.fill_pcfg_from_file(training_set_path)
     print('Trained PCFG')
 
-    for line in args.lines.split('\n'):
-        parsable, tree = pcfg.probabilitic_CKY(line.split())
+    parsed_tree_generator = generate_parsed_tree(pcfg,
+                                                 lines_to_parse.split('\n'))
+    for parsable, tree in parsed_tree_generator:
         if parsable:
-            print(' '.join(str(tree).split()))
+            if pprint:
+                tree.pretty_print()
+            else:
+                print(get_tree_sequoia_format(tree))
         else:
-            print('NOT PARSED : {}'.format(line))
+            print('NOT PARSED : {}'.format(tree))
 
 
-def parse_from_file(args):
+@main.command()
+@click.argument('input_file', type=click.File())
+@click.option(
+    '--training_set_path',
+    type=click.Path(),
+    default=path.join(SCRIPT_PATH, './data/train.tag'),
+    help='Path to the training dataset.')
+@click.option(
+    '--output_file',
+    type=click.File(mode='w'),
+    default='results.txt',
+    help='Where to put the results.')
+def parse_from_file(input_file, output_file, training_set_path):
     pcfg = PCFG()
-    pcfg.fill_pcfg_from_file(args.train_path)
+    pcfg.fill_pcfg_from_file(training_set_path)
     print('Trained PCFG')
 
-    input_file = open(args.test_path, 'r')
-
-    if args.out_path is not None:
-        output_file = open(path.join(OUTPUT_FOLDER, 'out'), 'w')
-        for line in tqdm(input_file):
-            parsable, tree = pcfg.probabilitic_CKY(line.split())
-            if parsable:
-                output_file.write(' '.join(str(tree).split()) + '\n')
-            else:
-                output_file.write('()\n')
-                print('NOT PARSED : {}'.format(line))
-        output_file.close()
-
-    else:
-        for line in input_file:
-            parsable, tree = pcfg.probabilitic_CKY(line.split())
-            if parsable:
-                print(' '.join(str(tree).split()))
-            else:
-                print('NOT PARSED : {}'.format(line))
+    parsed_tree_generator = generate_parsed_tree(pcfg, input_file)
+    for parsable, tree in tqdm(parsed_tree_generator):
+        if parsable:
+            output_file.write(get_tree_sequoia_format(tree) + '\n')
+        else:
+            output_file.write('()\n')
+            print('NOT PARSED : {}'.format(tree))
 
     input_file.close()
+    output_file.close()
+
+
+def get_tree_sequoia_format(tree):
+    return (' '.join(str(tree).split()))
+
+
+def generate_parsed_tree(pcfg, lines_to_parse):
+    for line in lines_to_parse:
+        parsable, tree = pcfg.probabilitic_CKY(line.split())
+        yield parsable, tree
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        '--train_path',
-        type=str,
-        default=path.join(SCRIPT_PATH, './data/train.tag'),
-        help='Path to the training dataset')
-
-    subparsers = parser.add_subparsers(help='sub-command help')
-
-    parser_std = subparsers.add_parser('std', help='Parse from standard input')
-    parser_std.add_argument('lines', type=str, help='Sentence to parse')
-
-    parser_std.set_defaults(func=parse_from_std)
-
-    # create the parser for the "b" command
-    parser_file = subparsers.add_parser('file', help='Parse file')
-
-    parser_file.add_argument(
-        'test_path', type=str, help='Path to the test dataset')
-    parser_file.add_argument(
-        '--out_path',
-        type=str,
-        default=None,
-        help='Path where the results are written.')
-
-    parser_file.set_defaults(func=parse_from_file)
-
-    args = parser.parse_args()
-    args.func(args)
+    main()
